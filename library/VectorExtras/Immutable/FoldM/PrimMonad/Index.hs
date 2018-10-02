2b477e49c96ec076528937c7f4da1432b99c8788
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-10-02 16:54:49 +0300

    Add a warning to the docs

--- library/VectorExtras/Immutable/FoldM/PrimMonad/Index.hs
+++ library/VectorExtras/Immutable/FoldM/PrimMonad/Index.hs
@@ -14,6 +14,8 @@ type IndexPrimMonadFoldM result = forall m. PrimMonad m => FoldM m Int result
 {-|
 Given the size of the vector, construct a fold, which produces a vector of
 frequencies of each index. I.e., the counts of how often it appeared.
+
+It is your responsibility to ensure that the indices are within the size of the vector produced.
 -}
 frequency :: (Vector vector count, Enum count) => Int -> IndexPrimMonadFoldM (vector count)
 frequency amount = FoldM step init extract where
