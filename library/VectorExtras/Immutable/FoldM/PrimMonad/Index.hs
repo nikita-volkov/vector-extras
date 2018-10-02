c8cd4f9523dfd0d77a71e150b4478212ed8c8009
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-10-02 16:50:26 +0300

    Rename counts to frequency and add docs

--- library/VectorExtras/Immutable/FoldM/PrimMonad/Index.hs
+++ library/VectorExtras/Immutable/FoldM/PrimMonad/Index.hs
@@ -6,10 +6,17 @@ import Control.Foldl
 import qualified Data.Vector.Generic.Mutable as Mutable
 
 
+{-|
+Fold on indices in PrimMonad.
+-}
 type IndexPrimMonadFoldM result = forall m. PrimMonad m => FoldM m Int result
 
-counts :: (Vector vector count, Enum count) => Int -> IndexPrimMonadFoldM (vector count)
-counts amount = FoldM step init extract where
+{-|
+Given the size of the vector, construct a fold, which produces a vector of
+frequencies of each index. I.e., the counts of how often it appeared.
+-}
+frequency :: (Vector vector count, Enum count) => Int -> IndexPrimMonadFoldM (vector count)
+frequency amount = FoldM step init extract where
   init = Mutable.new amount
   step mv index = Mutable.modify mv succ index $> mv
   extract = unsafeFreeze
