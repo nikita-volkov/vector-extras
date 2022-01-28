3f32814e3e187bf75cd8efc03e26eb5ee2e5c757
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-29 00:09:41 +0300

    Rename and address the arg ordering standard of sepBy

--- library/VectorExtras/Combinators.hs
+++ library/VectorExtras/Combinators.hs
@@ -9,26 +9,42 @@ import qualified VectorExtras.Accumulator as Acc
 import VectorExtras.Combinators.Helpers
 import VectorExtras.Prelude
 
+-- * Common
+
 many :: (MonadPlus m, Vector v a) => m a -> m (v a)
 many getElement =
   accMany Acc.init getElement <&> Acc.toVector
 
 sepBy :: (MonadPlus m, Vector v a) => m a -> m sep -> m (v a)
-sepBy getElement getSeparator =
-  orPure Vector.empty $ sepBy1 getElement getSeparator
+sepBy = flip sep
 
 sepBy1 :: (MonadPlus m, Vector v a) => m a -> m sep -> m (v a)
-sepBy1 getElement getSeparator =
+sepBy1 = flip sep1
+
+-- *
+
+-- |
+-- Same as 'sepBy' but with arguments flipped.
+-- Because the order of arguments in 'sepBy' does not feel right.
+sep :: (MonadPlus m, Vector v a) => m sep -> m a -> m (v a)
+sep getSeparator getElement =
+  orPure Vector.empty $ sep1 getSeparator getElement
+
+-- |
+-- Same as 'sepBy1' but with arguments flipped.
+-- Because the order of arguments in 'sepBy1' does not feel right.
+sep1 :: (MonadPlus m, Vector v a) => m sep -> m a -> m (v a)
+sep1 getSeparator getElement =
   do
     element <- getElement
     Acc.toVector <$> accMany (Acc.add element Acc.init) (getSeparator *> getElement)
 
-sepFollowed :: (MonadPlus m, Vector v a) => m sep -> m end -> m a -> m (v a)
-sepFollowed getSeparator getEnd getElement =
-  orPure Vector.empty $ sepFollowed1 getSeparator getEnd getElement
+sepEnd :: (MonadPlus m, Vector v a) => m sep -> m end -> m a -> m (v a)
+sepEnd getSeparator getEnd getElement =
+  orPure Vector.empty $ sepEnd1 getSeparator getEnd getElement
 
-sepFollowed1 :: (MonadPlus m, Vector v a) => m sep -> m end -> m a -> m (v a)
-sepFollowed1 getSeparator getEnd getElement =
+sepEnd1 :: (MonadPlus m, Vector v a) => m sep -> m end -> m a -> m (v a)
+sepEnd1 getSeparator getEnd getElement =
   do
     element <- getElement
     acc <- collect (Acc.add element Acc.init)
