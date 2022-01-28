74ca82466b4f09cb30c38b9cd9f99213e483902e
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-28 23:45:35 +0300

    Implement many and sepby

--- library/VectorExtras/Combinators.hs
+++ library/VectorExtras/Combinators.hs
@@ -3,13 +3,17 @@
 module VectorExtras.Combinators where
 
 import Data.Vector.Generic (Vector)
+import VectorExtras.Accumulator (Accumulator)
 import qualified VectorExtras.Accumulator as Acc
 import VectorExtras.Combinators.Helpers
 import VectorExtras.Prelude
 
 many :: (MonadPlus m, Vector v a) => m a -> m (v a)
-many f =
-  collect Acc.init <&> Acc.toVector
-  where
-    collect !acc =
-      orPure acc $ f >>= collect . flip Acc.add acc
+many getElement =
+  accMany Acc.init getElement <&> Acc.toVector
+
+sepBy :: (MonadPlus m, Vector v a) => m a -> m a -> m (v a)
+sepBy getElement getSeparator =
+  do
+    element <- getElement
+    Acc.toVector <$> accMany (Acc.add element Acc.init) (getSeparator *> getElement)
--- library/VectorExtras/Combinators/Helpers.hs
+++ library/VectorExtras/Combinators/Helpers.hs
@@ -1,7 +1,20 @@
 module VectorExtras.Combinators.Helpers where
 
+import VectorExtras.Accumulator (Accumulator)
+import qualified VectorExtras.Accumulator as Acc
 import VectorExtras.Prelude
+
+-- * General
 
 orPure :: (Alternative m) => a -> m a -> m a
 orPure res main =
   main <|> pure res
+
+-- * Accumulator-oriented
+
+accMany :: (MonadPlus m) => Accumulator a -> m a -> m (Accumulator a)
+accMany acc getElement =
+  collect acc
+  where
+    collect !acc =
+      orPure acc $ getElement >>= collect . flip Acc.add acc
