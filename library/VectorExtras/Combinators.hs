3671b4f65fa304375c0cd6bed100780c174d72a1
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-28 23:57:32 +0300

    Fix sepBy and add sepBy1

--- library/VectorExtras/Combinators.hs
+++ library/VectorExtras/Combinators.hs
@@ -3,6 +3,7 @@
 module VectorExtras.Combinators where
 
 import Data.Vector.Generic (Vector)
+import qualified Data.Vector.Generic as Vector
 import VectorExtras.Accumulator (Accumulator)
 import qualified VectorExtras.Accumulator as Acc
 import VectorExtras.Combinators.Helpers
@@ -12,8 +13,12 @@ many :: (MonadPlus m, Vector v a) => m a -> m (v a)
 many getElement =
   accMany Acc.init getElement <&> Acc.toVector
 
-sepBy :: (MonadPlus m, Vector v a) => m a -> m b -> m (v a)
+sepBy :: (MonadPlus m, Vector v a) => m a -> m sep -> m (v a)
 sepBy getElement getSeparator =
+  orPure Vector.empty $ sepBy1 getElement getSeparator
+
+sepBy1 :: (MonadPlus m, Vector v a) => m a -> m sep -> m (v a)
+sepBy1 getElement getSeparator =
   do
     element <- getElement
     Acc.toVector <$> accMany (Acc.add element Acc.init) (getSeparator *> getElement)
