bedcabe67e108279a7bf04f93ad4ea4e25d86eb2
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-12-10 23:08:49 +0300

    Add fromFoldableNarrowing

--- library/VectorExtras/Generic.hs
+++ library/VectorExtras/Generic.hs
@@ -1,6 +1,7 @@
 module VectorExtras.Generic
   ( module Basics,
     fromFoldable,
+    fromFoldableNarrowing,
   )
 where
 
@@ -9,6 +10,16 @@ import qualified VectorExtras.Accumulator as VAcc
 import VectorExtras.Basics.Generic as Basics
 import VectorExtras.Prelude
 
+{-# INLINE fromFoldable #-}
 fromFoldable :: (Foldable f, Vector v a) => f a -> v a
 fromFoldable =
   VAcc.toVector . foldl' (flip VAcc.add) VAcc.init
+
+{-# INLINE fromFoldableNarrowing #-}
+fromFoldableNarrowing :: (Foldable f, Vector v b) => (a -> Maybe b) -> f a -> v b
+fromFoldableNarrowing narrow =
+  VAcc.toVector . foldl' step VAcc.init
+  where
+    step acc el = case narrow el of
+      Nothing -> acc
+      Just narrowed -> VAcc.add narrowed acc
